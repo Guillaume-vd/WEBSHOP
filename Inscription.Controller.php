b791487e6dc19848c0634ebe82467a513e62c06a
Guillaume-vd <58851869+Guillaume-vd@users.noreply.github.com>
2020-01-22 10:20:09 +0100

    return

--- Inscription.Controller.php
+++ Inscription.Controller.php
@@ -6,13 +6,13 @@
 	
 	$sql_customers = "INSERT INTO customers (id, forname, surname, add1, add2, add3, postcode, phone, email, registered) 
 	VALUES ('NULL', '".$_POST['forname']."', '". $_POST['login']."', 'ligne adresse1', 'ligne adresse 2', '".$_POST['add3']."', '".$_POST['postcode']."', '".$_POST['phone']."', '".$_POST['email']."', 1)";
-	echo $sql_customers;
+	//echo $sql_customers;
 	$result = mysqli_query($cnxDb, $sql_customers) or die("erreur customer");
 	
 	$sql_logins = "INSERT INTO logins (id, customer_id, username, password)
 	VALUES ('NULL', 'NULL', '".$_POST['login']."', '".$_POST['mdp']."')";
-	echo $sql_logins;
+	//echo $sql_logins;
 	$result = mysqli_query($cnxDb, $sql_logins) or die("erreur logins");;
 	
-	//header('Location: accueil.php');
+	header('Location: accueil.php');
 ?>
